4c936582971321c64f54c108bc8e3c862ca2980e
panjiprann <panjipranantya20@gmail.com>
2024-03-20 22:20:58 +0700

    mo selese

--- jawa.cpp
+++ jawa.cpp
@@ -4,6 +4,27 @@ using namespace std;
 int arr[20];
 int n;
 
+void input(){
+    while (true)
+    {
+        cout <<"masukan banyaknya elemen array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 eleman.\n";
+        }
+    }
+    cout << endl;
+    cout << "====================" << endl;
+    cout << "masukan elemen array" << endl;
+    cout << "====================" << endl;
+    for (int i = 0; i < n; i++)
+{
+        cout << "data ke-" << (i + 1 ) << ": ";
+        cin >> arr[i];
+    }
+}
 void sortir(){
     int pass= 1;
     do {
@@ -18,7 +39,19 @@ void sortir(){
     } while ( pass <= n - 1);
     
 }
-
+void display (){
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "=================================" << endl;
+    for( int j = 0; j < n; j++){
+        cout << arr[j] << endl; //output setiap element array pada garis baru
+    }
+    cout << "jumlah pass = " << n-1  << endl;
+    cout << endl;  
+}
 int main(){
-    
+    input();
+    sortir();
+    display();
 }
