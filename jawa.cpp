cf4a854ac60e7473644f4ed1186d53961832ab18
panjiprann <panjipranantya20@gmail.com>
2024-03-20 22:11:50 +0700

    bikin algo sortir

--- jawa.cpp
+++ jawa.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+int arr[20];
+int n;
 
+void sortir(){
+    int pass= 1;
+    do {
+        for (int i = 0; i <= n -1 - pass ; i++){
+            if (arr[i] > arr [i + 1]){
+            int temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
+            }
+        }
+        pass = pass + 1;
+    } while ( pass <= n - 1);
+    
+}
+
+int main(){
+    
 }
